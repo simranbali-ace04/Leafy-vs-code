5d799348aff9bb0195ae1e9ee3d8184e73a7bfb3
Simran Bali <simranbali2006@gmail.com>
2026-06-24 13:34:29 +0530

    feat: initial extension setup and sidebar registration

--- leafy/src/extension.js
+++ leafy/src/extension.js
@@ -12,23 +12,26 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "leafy" is now active!');
+	console.log('🌱 Leafy has successfully sprouted and is running in the background!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('leafy.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	// const disposable = vscode.commands.registerCommand('leafy.helloWorld', function () {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Leafy!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from Leafy!');
+	// });
 
-	context.subscriptions.push(disposable);
+// 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	console.log('🍂 Leafy is going to sleep...');
+	
+}
 
 module.exports = {
 	activate,
